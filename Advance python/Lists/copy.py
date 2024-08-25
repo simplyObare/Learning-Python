a95d604ab7e198655958803248e9f9b7ed65eebc
simplyObare <josephatowiny@gmail.com>
2024-08-25 20:23:09 +0300

    lists in python

---
+++ Advance python/Lists/copy.py
@@ -0,0 +1,6 @@
+list_org = [10, 20, 30, 40, 50]
+list_cpy = list_org.copy()
+list_cpy.append(60)
+
+print(list_org)
+print(list_cpy)
---
+++ Advance python/Lists/lists.py
@@ -0,0 +1,11 @@
+myList = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+a = myList[2:5]
+print(a)
+b = myList[-4:-1]
+print(b)
+c = myList[2:]
+print(c)
+d = myList[:4]
+print(d)
+e = myList[:]
+print(e)
